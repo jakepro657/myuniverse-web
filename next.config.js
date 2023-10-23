550de13b01eb1eb5aa6065f874580292dde769bc
jakepro657 <jakepro657@gmail.com>
2023-10-24 00:05:15 +0900

    etc: config

--- next.config.js
+++ next.config.js
@@ -1,5 +1,15 @@
 /** @type {import('next').NextConfig} */
 const nextConfig = {
+  images: {
+    remotePatterns: [
+      {
+        protocol: 'https',
+        hostname: process.env.NEXT_PUBLIC_IMG_HOST,
+        port: '',
+        pathname: `/${process.env.NEXT_PUBLIC_IMG_PATH}/**`,
+      },
+    ],
+  },
     async redirects() {
         return [
           {
