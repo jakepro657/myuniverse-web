f98f616989aff16fcd4d18bb46b9ee6313bcd431
jakepro657 <jakepro657@gmail.com>
2023-10-23 17:01:36 +0900

    design: post create page ui modified

--- src/components/main/Main.tsx
+++ src/components/main/Main.tsx
@@ -6,18 +6,24 @@ import { useQuery } from "react-query";
 
 type Props = {};
 
-
 const Main = (props: Props) => {
-
   const { data } = useQuery(["posts"], useGetPosts);
 
   return (
-    <div className="p-4 bg-gray-50">
+    <div className="bg-gray-50">
+      <header className="p-4">
+        <div className="flex items-center w-full h-16 drop-shadow-sm">
+          <div className="text-2xl ml-8 font-bold">Deverse</div>
+
+          <Link
+            href={"/post"}
+            className="bg-black text-white p-2 rounded-3xl text-xl ml-auto mr-4 font-bold"
+          >
+            &nbsp;글쓰기 +&nbsp;
+          </Link>
+        </div>
+      </header>
       <main className="p-16 items-center w-full flex min-h-screen flex-col flex-nowrap">
-        <header>
-          <div className="text-4xl font-bold">Jake Yubin Kim's Devlog</div>
-          <Link href={"/post"}>글쓰기</Link>
-        </header>
         <div className="mt-16"></div>
         <div className="flex flex-wrap justify-center">
           {data?.map((item: any, i: number) => {
@@ -26,7 +32,7 @@ const Main = (props: Props) => {
                 id={i + 1}
                 key={i}
                 title={item.title}
-                username={item.authorId}
+                username={item.author.email}
                 profile="/next.svg"
                 comments={0}
                 content={item.content}
